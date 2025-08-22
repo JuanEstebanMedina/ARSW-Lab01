0b8bc6bbe490692c7010c2ea5fb39b86b7f0b9de
JuanEstebanMedina <juan.medina-r@mail.escuelaing.edu.co>
2025-08-22 09:46:02 -0500

    feat: PartI CountThread Class implementation
    Co-authored-by: Hakki17 <mariapaulasm2018@gmail.com>

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -9,6 +9,25 @@ package edu.eci.arsw.threads;
  *
  * @author hcadavid
  */
-public class CountThread {
+public class CountThread extends Thread{
+
+    private int A, B;
+    CountThread(int a, int b){
+        this.A = a;
+        this.B = b;
+    }
+
+    public void run() {
+        System.out.println("\nCountThread is running...");
+        
+        for (int i = A ; i <= B; i++){
+            if (i % 10 == 0){
+                System.out.println("");
+            }
+            System.out.print(i + " | ");
+        }
+
+        System.out.println("\nCountThread finished...");
+    }
     
 }
